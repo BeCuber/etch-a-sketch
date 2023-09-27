a81235745209b7114b7733795feb4eb94567db30
Feliniberto <136160356+Feliniberto@users.noreply.github.com>
2023-09-27 18:28:20 +0200

    Add loop to generate multiples div,s in a row

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 const container = document.getElementById('container');
 
 
-const newDiv = document.createElement('div');
-newDiv.className = 'square';
-container.appendChild(newDiv);
+
+//crear 16 div's
+let newArrayDiv = [];
+for (let i = 0; i<16; i++){
+    let newDiv = document.createElement('div');
+    newDiv.className = 'square'
+    newDiv.id = [i]; //posiblemente innecesario que tengan un id
+    newArrayDiv.push(newDiv);
+};
+
+for (let i = 0; i<newArrayDiv.length; i++){
+    container.appendChild(newArrayDiv[i]);
+};
+
+console.log(newArrayDiv);
 
 console.log(container);
