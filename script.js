f1eddb57c83c6b1e3de5c68d914f9a510b49444e
Feliniberto <136160356+Feliniberto@users.noreply.github.com>
2023-09-27 19:22:20 +0200

    Add multiples columns in one row

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ const container = document.getElementById('container');
 
 
 
-//crear 16 div's
-let newArrayDiv = [];
+//crear 16 div's, se almacenan en un array
+let rowArrayDiv = [];
 for (let i = 0; i<16; i++){
-    let newDiv = document.createElement('div');
-    newDiv.className = 'square'
-    newDiv.id = [i]; //posiblemente innecesario que tengan un id
-    newArrayDiv.push(newDiv);
+    let rowDiv = document.createElement('div');
+    rowDiv.className = 'row';
+    rowDiv.id = [i]; //posiblemente innecesario que tengan un id
+    rowArrayDiv.push(rowDiv);
 };
 
-for (let i = 0; i<newArrayDiv.length; i++){
-    container.appendChild(newArrayDiv[i]);
+//se anexan los elementos del array en el container
+
+for (let i = 0; i<rowArrayDiv.length; i++){
+    container.appendChild(rowArrayDiv[i]);
+};
+console.log(container);//borrable
+
+//crear 16 div en el primero del anterior
+let firstDiv = document.getElementById("0");
+
+let colArrayDiv = [];
+for (let i=0; i<16; i++){
+    let colDiv = document.createElement('div');
+    colDiv.className = 'column';
+    colArrayDiv.push(colDiv);
 };
 
-console.log(newArrayDiv);
+console.log(rowArrayDiv);//borrable
+console.log(colArrayDiv); //borrable
 
-console.log(container);
+for (let i=0; i<colArrayDiv.length; i++){
+    firstDiv.appendChild(colArrayDiv[i]);
+};
+console.log(firstDiv);
+
+
+
+
