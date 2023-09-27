1cbd278a51d35a029d4ab6071ef32e166f6aa5df
Feliniberto <136160356+Feliniberto@users.noreply.github.com>
2023-09-27 13:39:43 +0200

    Add links in html head, background style in css and create 1 div in js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const container = document.getElementById('container');
+
+
+const newDiv = document.createElement('div');
+newDiv.className = 'square';
+container.appendChild(newDiv);
+
+console.log(container);
