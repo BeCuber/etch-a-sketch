3a9cc9bc1b1b24af8c78f346e9dd6fe8949a2985
Feliniberto <136160356+Feliniberto@users.noreply.github.com>
2023-09-28 18:30:07 +0200

    Add erase function in clear button

--- script.js
+++ script.js
@@ -2,41 +2,55 @@
 
 
 //declare number of squares per side
-let squareNumber = 16;
+let squareNumber = document.getElementById('numberChosen').value;
+
+//let squareNumber = 32;
 
 //get container
 const container = document.getElementById('container');
 
-//create an array to store 16 div for the first row
-let rowArrayDiv = [];
+function createGrid(squareNumber){
+    clearGrid();
+    //create an array to store 16 div for the first row
+    let rowArrayDiv = [];
 
-for (let i = 0; i<squareNumber; i++){
-    let rowDiv = document.createElement('div'); //create 1 div
-    rowDiv.className = 'row'; //add classes to each div
-    rowDiv.id = [i]; //add id to each div
-    rowArrayDiv.push(rowDiv);
-};
-
-//appends each item from row array inside the container
-for (let i = 0; i<rowArrayDiv.length; i++){
-    container.appendChild(rowArrayDiv[i]);
-
-    //create an array with same length as row-array for each row-div
-    let colArrayDiv = [];
-    for (let k=0; k<rowArrayDiv.length; k++){
-        let colDiv = document.createElement('div');
-        colDiv.className = 'column';
-        colArrayDiv.push(colDiv);
+    for (let i = 0; i<squareNumber; i++){
+        let rowDiv = document.createElement('div'); //create 1 div
+        rowDiv.className = 'row'; //add classes to each div
+        rowDiv.id = [i]; //add id to each div
+        rowArrayDiv.push(rowDiv);
     };
+
+    //appends each item from row array inside the container
+    for (let i = 0; i<rowArrayDiv.length; i++){
+        container.appendChild(rowArrayDiv[i]);
+
+        //create an array with same length as row-array for each row-div
+        let colArrayDiv = [];
+        for (let k=0; k<rowArrayDiv.length; k++){
+            let colDiv = document.createElement('div');
+            colDiv.className = 'column';
+            colArrayDiv.push(colDiv);
+        };
+        
+        //get items from row-array    
+        let firstDiv = document.getElementById([i]);
+        //append each col-array to each row-div
+        for (let j=0; j<rowArrayDiv.length; j++){
+            firstDiv.appendChild(colArrayDiv[j]);
+        };
+
+    };
+
+};
+function clearGrid(){
     
-    //get items from row-array    
-    let firstDiv = document.getElementById([i]);
-    //append each col-array to each row-div
-    for (let j=0; j<rowArrayDiv.length; j++){
-        firstDiv.appendChild(colArrayDiv[j]);
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
     };
-
 };
+
+createGrid(squareNumber);
 
 
 //** EVENTS LISTENERS **//
@@ -54,12 +68,32 @@ function changeColor(e){
 };
 
 //------Input------//
-let numberChosen = document.getElementsByTagName("input");
-numberChosen.textContent = "hello";
 
+/*
+let numberChosen = document.getElementById("numberChosen");
 
+numberChosen.addEventListener('keydown', changeSide);
+function changeSide(e){
+    if (e.key === "Enter"){
+        squareNumber = numberChosen.value;
+        createGrid(squareNumber);
+    };
+    
+};
+*/
 
+//------CLEAR BUTTON------//
+ let clearBtn = document.getElementById('clearBtn');
 
+ clearBtn.addEventListener('click', erase);
+ 
+ function erase(e){
+    let colArrayDiv = document.getElementsByClassName('column');
+    
+    for (let i=0; i<colArrayDiv.length; i++){
+        colArrayDiv[i].style.backgroundColor = 'rgb(176, 243, 167)';
+    };
+ };
 
 
 
