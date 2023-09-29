e100353614ff4d6366ce3bb8e7a9a3cc9ec7abea
Feliniberto <136160356+Feliniberto@users.noreply.github.com>
2023-09-29 23:25:26 +0200

    Finish input box and its validate properties

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
-//** CREATE THE GRID **/
-
-
-//declare number of squares per side
-let squareNumber = document.getElementById('numberChosen').value;
-
-//let squareNumber = 32;
-
 //get container
 const container = document.getElementById('container');
 
+//get input textbox
+let numberChosen = document.getElementById('numberChosen');
+
+//declare number of squares per side by default 
+let squareNumber = numberChosen.value;
+
+//create a Grid by default
+createGrid(squareNumber);
+
+//------HANDLING INPUT------//
+
+//change color if value invalid
+numberChosen.addEventListener('keyup', validation);
+
+function validation(){
+    
+    squareNumber = numberChosen.value;
+
+    if (squareNumber > 0 && squareNumber <= 100){
+        numberChosen.style.color = "black";
+        
+    }else{
+        numberChosen.style.color = "red";
+    };
+};
+
+
+//run changeSide if && value is valid
+numberChosen.addEventListener('keydown', changeSide);
+
+function changeSide(e){
+
+    squareNumber = numberChosen.value;
+    if (e.key == "Enter" && squareNumber > 0 && squareNumber <= 100){
+        
+        createGrid(squareNumber); //create a new grid
+        
+    }; 
+};
+
+
+//** CREATE THE GRID **/
+
+
 function createGrid(squareNumber){
-    clearGrid();
-    //create an array to store 16 div for the first row
+
+    //clear Grid to let container get the new div,s
+
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    };
+
+    //create an array to store div,s for the first row
+
     let rowArrayDiv = [];
 
     for (let i = 0; i<squareNumber; i++){
         let rowDiv = document.createElement('div'); //create 1 div
         rowDiv.className = 'row'; //add classes to each div
         rowDiv.id = [i]; //add id to each div
-        rowArrayDiv.push(rowDiv);
+        rowArrayDiv.push(rowDiv); //add each div to the array
     };
 
     //appends each item from row array inside the container
+
     for (let i = 0; i<rowArrayDiv.length; i++){
         container.appendChild(rowArrayDiv[i]);
 
@@ -29,8 +73,8 @@ function createGrid(squareNumber){
         let colArrayDiv = [];
         for (let k=0; k<rowArrayDiv.length; k++){
             let colDiv = document.createElement('div');
-            colDiv.className = 'column';
-            colArrayDiv.push(colDiv);
+            colDiv.className = 'column'; //add 'column' class to every square
+            colArrayDiv.push(colDiv);//got array of arrays
         };
         
         //get items from row-array    
@@ -39,68 +83,41 @@ function createGrid(squareNumber){
         for (let j=0; j<rowArrayDiv.length; j++){
             firstDiv.appendChild(colArrayDiv[j]);
         };
-
     };
 
+    //add paint function to the grid
+    paint();
 };
-function clearGrid(){
-    
-    while (container.hasChildNodes()){
-        container.removeChild(container.firstChild);
+
+
+//------MOUSE PAINTING------//
+
+function paint(){
+    let squareTarget = document.getElementsByClassName('column');
+
+    for (let i=0; i<squareTarget.length; i++){
+        squareTarget[i].addEventListener('mouseover', changeColor);
+    };
+
+    function changeColor(e){
+        e.target.style.backgroundColor = 'green';
     };
 };
 
-createGrid(squareNumber);
 
-
-//** EVENTS LISTENERS **//
-
-//------Mouse Painting------//
-
-let squareTarget = document.getElementsByClassName('column');
-
-for (let i=0; i<squareTarget.length; i++){
-    squareTarget[i].addEventListener('mouseover', changeColor);
-};
-
-function changeColor(e){
-    e.target.style.backgroundColor = 'green';
-};
-
-//------Input------//
-
-/*
-let numberChosen = document.getElementById("numberChosen");
-
-numberChosen.addEventListener('keydown', changeSide);
-function changeSide(e){
-    if (e.key === "Enter"){
-        squareNumber = numberChosen.value;
-        createGrid(squareNumber);
-    };
-    
-};
-*/
 
 //------CLEAR BUTTON------//
- let clearBtn = document.getElementById('clearBtn');
 
- clearBtn.addEventListener('click', erase);
+let clearBtn = document.getElementById('clearBtn');
+
+clearBtn.addEventListener('click', erase);
  
- function erase(e){
-    let colArrayDiv = document.getElementsByClassName('column');
+function erase(){
+   let colArrayDiv = document.getElementsByClassName('column');
     
-    for (let i=0; i<colArrayDiv.length; i++){
-        colArrayDiv[i].style.backgroundColor = 'rgb(176, 243, 167)';
-    };
- };
-
-
-
-
-
-
-
-
+   for (let i=0; i<colArrayDiv.length; i++){
+       colArrayDiv[i].style.backgroundColor = 'rgb(176, 243, 167)';
+   };
+};
 
 
